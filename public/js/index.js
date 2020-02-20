098a2bf5b7c8342bcb722d949e216e4d29d1aa33
Jordan Raji Lazo Cahua <jordanraji@gmail.com>
2020-02-20 15:06:13 -0500

    Ads added

--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,20 @@ class Item extends React.Component {
     render() {
         return (
             <div className="col-md-3 col-6 my-3">
-                <div className="card card-body h-100">
-                    <p className="text-center my-4" style = {{ fontSize: this.props.product.size }}>{this.props.product.face}</p>
-                    <div className="mt-auto">
-                        <h5 className="text-center text-red">${ this.priceHandler(this.props.product.price) }</h5>
-                        <p className="text-center text-secondary"><small>{ this.dateHandler(this.props.product.date) }</small></p>
+                { !this.props.product.ad ?
+                    <div className="card card-body h-100">
+                        <p className="text-center my-4" style={{ fontSize: this.props.product.size }}>{this.props.product.face}</p>
+                        <div className="mt-auto">
+                            <h5 className="text-center text-red">${this.priceHandler(this.props.product.price)}</h5>
+                            <p className="text-center text-secondary"><small>{this.dateHandler(this.props.product.date)}</small></p>
+                        </div>
                     </div>
-                </div>
+                    : 
+                    <div className="card card-body h-100">
+                        <img class="ad" src={"/ads/?r="+ Math.floor(Math.random() * 1000)} />
+                    </div>
+                }
+
             </div>
         );
     }
@@ -43,13 +50,17 @@ class App extends React.Component {
             page: 0,
             prevY: 0,
             loading: false,
+            productsCompleted: false,
+            ad:{
+                ad: true
+            }
         };
 
         this.getProducts = this.getProducts.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    getProducts(page=0, limit=20, sort='id', addToProducts=false) {
+    getProducts(page=0, limit=19, sort='id', addToProducts=false) {
         this.setState({loading: true});
         axios({
             url: 'http://localhost:3000/products',
@@ -60,29 +71,46 @@ class App extends React.Component {
                 "_sort": sort
             }
         }).then(response => {
-            console.log(response);
+            const data = response.data
+            if(data.length == 0){
+                this.setState({
+                    productsCompleted: true
+                });
+                this.setState({ loading: false });
+                return;
+            }
+
+            let random_position = Math.floor(Math.random() * 18);
+            data.splice(random_position, 0, {ad:true})
 
             if(addToProducts){
                 this.setState({
-                    products: this.state.products.concat(response.data),
+                    products: this.state.products.concat(data),
                 });
             } else {
                 this.setState({
-                    products: response.data,
+                    products: data,
                 });
             }
             this.setState({ loading: false });
+        }).catch(error =>{
+            this.setState({ loading: false });
+            alert("Cannot connect to the server. Try again");
         })
     }
 
     handleChange(){
-        this.setState({ orderBy: event.target.value });
+        this.setState({ 
+            orderBy: event.target.value,
+            page: 0,
+            productsCompleted: false,
+        });
         this.getProducts(0, undefined, event.target.value, false);
     }
 
     handleObserver(entities, observer) {
         const y = entities[0].boundingClientRect.y;
-        if (this.state.prevY > y) {
+        if (this.state.prevY > y && !this.state.productsCompleted) {
             this.setState({ page: this.state.page + 1 });
             this.getProducts(this.state.page + 1, undefined, this.state.orderBy , true);
         }
@@ -141,7 +169,15 @@ class App extends React.Component {
                                 <span className="sr-only">Loading...</span>
                             </div>
                         </div>
-                        : ""}
+                        : ""
+                    }
+
+                    {this.state.productsCompleted ?
+                        <div className="text-center">
+                            <h6 className="text-red">~ end of catalogue ~</h6>
+                        </div>
+                        : ""
+                    }
 
                     <div ref={loadingRef => (this.loadingRef = loadingRef)} style={loadingCSS}>
                     </div>
