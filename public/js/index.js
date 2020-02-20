593c14bb8cc284ab215f2e6c3e89d2455e429b6a
Jordan Raji Lazo Cahua <jordanraji@gmail.com>
2020-02-20 15:35:14 -0500

    Adding Comments

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,9 @@ class Item extends React.Component {
     render() {
         return (
             <div className="col-md-3 col-6 my-3">
-                { !this.props.product.ad ?
+                {   
+                    // IF PRODUCT
+                    !this.props.product.ad ?
                     <div className="card card-body h-100">
                         <p className="text-center my-4" style={{ fontSize: this.props.product.size }}>{this.props.product.face}</p>
                         <div className="mt-auto">
@@ -16,19 +18,21 @@ class Item extends React.Component {
                         </div>
                     </div>
                     : 
+                    // IF AD
                     <div className="card card-body h-100">
                         <img class="ad" src={"/ads/?r="+ Math.floor(Math.random() * 1000)} />
                     </div>
                 }
-
             </div>
         );
     }
 
+    // Function to convert cents to dollars
     priceHandler(cents) {
         return (cents/100.0).toFixed(2);
     }
 
+    // This function calculates the difference between the current date and the date when product was created
     dateHandler(date) {
         const product_date = new Date(date);
         const current_date = new Date();
@@ -45,21 +49,19 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            products: [],
-            orderBy: "id",
-            page: 0,
-            prevY: 0,
-            loading: false,
-            productsCompleted: false,
-            ad:{
-                ad: true
-            }
+            products: [], // List of products and ads
+            orderBy: "id", // How to order
+            page: 0, // Page for calling API
+            prevY: 0, // Needed for scrolling
+            loading: false, // Needed for API Request
+            productsCompleted: false, // true if there are no other products
         };
 
         this.getProducts = this.getProducts.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
+    // This function gets the requested products 
     getProducts(page=0, limit=19, sort='id', addToProducts=false) {
         this.setState({loading: true});
         axios({
@@ -72,6 +74,8 @@ class App extends React.Component {
             }
         }).then(response => {
             const data = response.data
+
+            // If there are no more products update productsCompleted
             if(data.length == 0){
                 this.setState({
                     productsCompleted: true
@@ -80,6 +84,7 @@ class App extends React.Component {
                 return;
             }
 
+            // Adding ad in a random position to products List
             let random_position = Math.floor(Math.random() * 18);
             data.splice(random_position, 0, {ad:true})
 
@@ -94,11 +99,13 @@ class App extends React.Component {
             }
             this.setState({ loading: false });
         }).catch(error =>{
+            // Server error prevention
             this.setState({ loading: false });
             alert("Cannot connect to the server. Try again");
         })
     }
 
+    // This function handles the change of the order of the products
     handleChange(){
         this.setState({ 
             orderBy: event.target.value,
@@ -108,6 +115,7 @@ class App extends React.Component {
         this.getProducts(0, undefined, event.target.value, false);
     }
 
+    // This function handles the scrolling
     handleObserver(entities, observer) {
         const y = entities[0].boundingClientRect.y;
         if (this.state.prevY > y && !this.state.productsCompleted) {
@@ -118,8 +126,10 @@ class App extends React.Component {
     }
 
     componentDidMount() {
+        // Get first products
         this.getProducts();
 
+        // Define variables for scrolling
         var options = {
             root: null,
             rootMargin: "0px",
@@ -158,24 +168,28 @@ class App extends React.Component {
                 </nav>
                 <div className="container">
                     <div className="row" style={{ minHeight: "80px" }}>
-                        {this.state.products.map(function (product, index) {
-                            return <Item key={index} product={product}></Item>
-                        })}
+                        {   /* Listing all products */
+                            this.state.products.map(function (product, index) {
+                                return <Item key={index} product={product}></Item>
+                            })
+                        }
                     </div>
 
-                    {this.state.loading? 
-                        <div className="text-center">
-                            <div className="spinner-border">
-                                <span className="sr-only">Loading...</span>
+                    {/* Loading spinner */
+                        this.state.loading? 
+                            <div className="text-center">
+                                <div className="spinner-border">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
                             </div>
-                        </div>
                         : ""
                     }
 
-                    {this.state.productsCompleted ?
-                        <div className="text-center">
-                            <h6 className="text-red">~ end of catalogue ~</h6>
-                        </div>
+                    { /* End of Catalogue */
+                        this.state.productsCompleted ?
+                            <div className="text-center">
+                                <h6 className="text-red">~ end of catalogue ~</h6>
+                            </div>
                         : ""
                     }
 
